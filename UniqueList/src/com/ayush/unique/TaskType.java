5045708a2a29f081d834967cd189969462c9c024
ayushoo7 <ayush7007@gmail.com>
2018-08-19 09:35:33 +0530

    Added code

---
+++ UniqueList/src/com/ayush/unique/TaskType.java
@@ -0,0 +1,60 @@
+package com.ayush.unique;
+
+public class TaskType {
+	
+	private int uid;
+	private String name;
+	
+	
+	public TaskType(int uid, String name) {
+		super();
+		this.uid = uid;
+		this.name = name;
+	}
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "TasjType [uid=" + uid + ", name=" + name + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + uid;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TaskType other = (TaskType) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (uid != other.uid)
+			return false;
+		return true;
+	}
+	
+	
+	
+
+}
